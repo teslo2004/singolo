6531d9bf9617433ccb158feba95280c21672f050
teslo2004 <teslo2004@gmail.com>
2020-03-16 19:32:33 +0200

    feat: add activation phone screen

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//Menu link
+
 const MENU = document.getElementById('header__menu');
 MENU.addEventListener('click', (e) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     e.target.classList.add('active');
 });
+
+//Slider
 
 const NEXT = document.getElementById('nextImg');
 const PREV = document.getElementById('prevImg');
@@ -25,4 +29,28 @@ function onPrevBtnClick(){
         FIRST.classList.toggle('slider__active');
         SECOND.classList.toggle('slider__active');
     } 
+}
+
+
+//Activation phone screen
+
+const VERTICAL = document.getElementById('vertical');
+const HORIZONTAL = document.getElementById('horizontal');
+let iphoneVertical = document.getElementById('iphone__vertical');
+let iphoneHorizontal = document.getElementById('iphone__horizontal');
+
+iphoneVertical.addEventListener('click', onClickVerticalScreen);
+
+function onClickVerticalScreen(){
+   if(iphoneVertical.click){
+       VERTICAL.classList.toggle('screen__off');
+   }  
+}
+
+iphoneHorizontal.addEventListener('click', onClickHorizontalScreen);
+
+function onClickHorizontalScreen(){
+   if(iphoneHorizontal.click){
+       HORIZONTAL.classList.toggle('screen__off');
+   }  
 }
