12acf1eb07f7e09315f95e9faa649800139414a1
teslo2004 <teslo2004@gmail.com>
2020-03-16 12:40:56 +0200

    feat: add slider script

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ MENU.addEventListener('click', (e) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     e.target.classList.add('active');
 });
+
+const NEXT = document.getElementById('nextImg');
+const PREV = document.getElementById('prevImg');
+var FIRST = document.getElementById('slide-1');
+var SECOND = document.getElementById('slide-2')
+
+NEXT.addEventListener('click', onNextBtnClick);
+PREV.addEventListener('click', onPrevBtnClick);
+
+
+function onNextBtnClick() {
+    if (NEXT.click) {
+        FIRST.classList.toggle('slider__active');
+        SECOND.classList.toggle('slider__active');
+    }    
+}
+
+function onPrevBtnClick(){
+    if (NEXT.click) {
+        FIRST.classList.toggle('slider__active');
+        SECOND.classList.toggle('slider__active');
+    } 
+}
